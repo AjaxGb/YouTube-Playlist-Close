285e53bd0e862087491a0567ccd3a78cb472d923
AjaxGb <ajaxgb@gmail.com>
2018-09-26 05:06:20 -0400

    Update to 2.0

--- YouTube Playlist Close.user.js
+++ YouTube Playlist Close.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         YouTube Playlist Close
-// @version      1.10
+// @version      2.0
 // @description  Allow quick closing of playlists
 // @author       AjaxGb
 // @match        http*://www.youtube.com/*
